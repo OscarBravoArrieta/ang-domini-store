0756c48955ac14552d5a2ae0a9cac52b950f3ff8
obravo <obravo@asercol.com>
2024-03-12 14:42:14 -0500

    Making shoppin cart

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+ import { CurrencyPipe } from '@angular/common';
+ import { Component, inject } from '@angular/core';
+ import { CartStore } from '@shared/store/shoping-cart.store';
+ import { CheckoutService } from './services/checkout.service';
 
-@Component({
-  selector: 'app-checkout',
-  standalone: true,
-  imports: [],
-  templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.scss'
-})
-export default class CheckoutComponent {
+ @Component({
+     selector: 'app-checkout',
+     standalone: true,
+     imports: [CurrencyPipe],
+     templateUrl: './checkout.component.html',
+     styleUrl: './checkout.component.scss'
+ })
+ export default class CheckoutComponent {
+
+     public cartStore = inject(CartStore)
+     private readonly checkoutService = inject(CheckoutService)
+
+     //--------------------------------------------------------------------------------------------
+
+     onProceedToPay () {
+
+         this.checkoutService.onProceedToPay()
 
 
-}
+     }
+
+     //--------------------------------------------------------------------------------------------
+
+     removeItem(id: number) {
+
+         this.cartStore.removeFromCart(id)
+
+     }
+
+     //--------------------------------------------------------------------------------------------
+
+     clearAll() {
+
+        this.cartStore.clearCart()
+
+     }
+     //--------------------------------------------------------------------------------------------
+
+
+ }
